8838ae2bee5963dd2556004e05319daaf81969a8
Fernando Alves <up201605270@fe.up.pt>
2018-11-07 11:29:34 +0000

    Circular animation done, but drawing has issues

--- Proj2/animations/CircularAnimation.js
+++ Proj2/animations/CircularAnimation.js
@@ -1,22 +1,36 @@
+var DEGREE_TO_RAD = Math.PI / 180;
+
 /**
- * LinearAnimation class, representing a linear animation.
+ *  class, representing a circular animation.
  */
-class LinearAnimation extends Animation {
+class CircularAnimation extends Animation {
     /**
      * @constructor
      */
     constructor(animationId, time, center,radius,startang,rotang) {
         super(animationId,time);
-        this.center = center;
+        console.log(center);
+        this.x_center = center[0];
+        this.y_center = center[1];
+        this.z_center = center[2];
         this.radius = radius;
-        this.startang = startang;
-        this.rotang = rotang;
-
+        this.startang = startang * DEGREE_TO_RAD;
+        this.rotang = rotang * DEGREE_TO_RAD;
+        this.initAnimation();
         
     }
 
     initAnimation(){
+        this.dist = Math.abs(this.rotang) * this.radius;
+        this.speed = this.dist/this.time;
+        if(this.rotang > 0){
+            this.ang_speed = this.speed / this.radius;
+        }
+        else{
+            this.ang_speed = -this.speed / this.radius;
+        }
 
+        this.elapsedTime = 0; //maybe temporary
 
     }
 
@@ -24,13 +38,29 @@ class LinearAnimation extends Animation {
      * updates the actual position of the animation
      */
     update(deltaTime){
-
+        
+        //maybe temporary, use dist instead of time?
+        this.elapsedTime += deltaTime;
+        if(this.elapsedTime >= this.time){
+            this.terminated = true;
+        }
 
     }
 
     getMatrix(deltaTime){
-
+        console.log("Lonely");
     }
 
-    apply(){}
+    apply(deltaTime){
+       
+        var transform = mat4.create();
+        mat4.identity(transform);
+
+        var deltaAng = this.startang + this.ang_speed * deltaTime;
+        mat4.translate(transform,transform,[this.x_center,this.y_center,this.z_center]);
+        mat4.rotate(transform, transform, deltaAng, [0, 1, 0]);
+        mat4.translate(transform, transform, [this.radius, 0, 0]);
+        
+        return transform;
+    }
 }
