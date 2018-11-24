109025d7afe7c12ae487d36d2324c8bb4fb67b80
Raidenkyu <joao-carlos.14@hotmail.com>
2018-11-24 12:41:42 +0000

    better ship movement

--- Proj2/animations/CircularAnimation.js
+++ Proj2/animations/CircularAnimation.js
@@ -52,11 +52,16 @@ class CircularAnimation extends Animation {
     apply(deltaTime){
        //console.log(this.elapsedAngle);
         var transform = mat4.create();
+        mat4.identity(transform);
+
         mat4.translate(transform, transform, [this.x_center,this.y_center,this.z_center]);
         mat4.rotate(transform, transform, this.elapsedAngle, [0, 1, 0]);
 
         mat4.translate(transform,transform,[this.radius,0,0]);
 
+        if(this.direction == 1){
+            mat4.rotate(transform,transform,Math.PI,[0,1,0]);
+        }
         
         return transform;
     }
